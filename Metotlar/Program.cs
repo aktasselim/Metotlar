e5912d45563c1d052601695926aee0d29dc45168
Selim <Selim@LAPTOP-HMVGVT1G>
2022-04-23 12:40:03 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Metotlar // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2, };
+
+            foreach (Urun urun in urunler)
+            {
+                
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati); 
+                Console.WriteLine(urun.Aciklama); 
+                Console.WriteLine("---------------");
+
+
+            }
+            Console.WriteLine("----------Metotlar------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Çilek", "Emiralem Çileği", 30);
+            
+
+
+
+
+
+
+        }
+        
+        
+        
+
+    }
+}
